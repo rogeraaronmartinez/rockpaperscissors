89522e92770d65d615138bc2faa5ab2579c3565a
Roger Martinez <rogeraaronmartinez@gmail.com>
2023-11-13 18:27:31 -0600

    Added UI for player to play 5 rounds and keep score.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,21 @@
+const rockButton = document.getElementById("rockButton");
+const paperButton = document.getElementById("paperButton");
+const scissorsButton = document.getElementById("scissorsButton");
+
+let userScore = 0;
+let computerScore = 0;
+let round = 0;
+
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const randomChoice = Math.floor(Math.random() * choices.length);
     return choices[randomChoice];
 }
 
-function playRound() {
+function playRound(playerChoice) {
     let result = "";
     let computerSelection = getComputerChoice();
-    let playerSelection = prompt();
+    let playerSelection = playerChoice;
     
 
     if (playerSelection == "rock" && computerSelection == "rock") {
@@ -29,18 +37,53 @@ function playRound() {
     }else if (playerSelection == "scissors" && computerSelection == "scissors") {
         result = "Tie, try again!";
     }
-    console.log(result);
-    console.log(playerSelection, computerSelection);
-    return result;
+    
+    console.log("Player Selection: " + playerSelection);
+    console.log("Computer Selection: " + computerSelection);
+    game(result);
 }
 
-function game() {
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound()
+function game(result) {
+    
+    const resultclass = document.querySelector(".results");
+    const scoreclass = document.querySelector(".score");
+    
+
+    resultclass.appendChild(document.createTextNode(result));
+    resultclass.appendChild(document.createElement("br"));
+    round++;
+    if (result === "You won!") {
+        userScore++;
+    }else if (result === "You lost!") {
+        computerScore++;
+    }
+
+    scoreclass.appendChild(document.createTextNode('Round ' + round + ': ' + 'Your Score: ' + userScore + '  Computer Score: ' + computerScore));
+    scoreclass.appendChild(document.createElement("br"));
+    console.log("Round: " +round);
+    console.log("User Score: " + userScore);
+    console.log("Computer Score: " + computerScore);
+    if (round === 5) {
+        endGame();
+    }   
+}
+
+function endGame() {
+    let finalResult;
+
+    if (userScore > computerScore) {
+        finalResult = "You win the game!";
+    }else if (userScore < computerScore) {
+        finalResult = "You lose the game!";
+    }else {
+        finalResult = "You tied the game!";
+    }
+    const winnerclass = document.querySelector(".winner");
+    winnerclass.appendChild(document.createTextNode('Game Over! ' + finalResult));
+    winnerclass.appendChild(document.createElement("br"));
+    winnerclass.appendChild(document.createTextNode('Your score: ' + userScore));
+    winnerclass.appendChild(document.createElement("br"));
+    winnerclass.appendChild(document.createTextNode('Computer score: ' + computerScore));
 }
 
 
-console.log(game());
